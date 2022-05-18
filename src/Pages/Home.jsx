6b03fa73717729ed0f1996253a997fe97f71a37e
tanvirch0750 <tanvir0750@yahoo.com>
2022-05-19 01:45:08 +0600

    implement completed functionality

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -7,7 +7,7 @@ import TaskForm from "../components/TaskForm";
 import auth from "../Firebase.init";
 
 const Home = () => {
-  const [user, loading] = useAuthState(auth);
+  const [user] = useAuthState(auth);
   const [deleteModal, setDeleteModal] = useState(null);
   const {
     isLoading,
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Task = ({ task, setDeleteModal }) => {
   const { name, description } = task;
+  const [lineThrough, setLineThrough] = useState(false);
+  const [disable, setDisable] = useState(false);
+
+  const handleComplete = () => {
+    setLineThrough(true);
+    alert(`Congratulations you completed "${name}" task`);
+    setDisable(true);
+  };
 
   return (
     <tr className="bg-neutral">
-      <td className="bg-neutral">{name}</td>
+      <td className={`bg-neutral ${lineThrough && "line-through text-accent"}`}>
+        {name}
+      </td>
       <td className="bg-neutral">{description}</td>
       <td className="bg-neutral">
-        <button className="btn btn-sm bg-success text-gray-900 hover:text-white">
+        <button
+          onClick={handleComplete}
+          className="btn btn-sm bg-success text-gray-900 hover:text-white"
+          disabled={disable}
+        >
           Completed
         </button>
       </td>
